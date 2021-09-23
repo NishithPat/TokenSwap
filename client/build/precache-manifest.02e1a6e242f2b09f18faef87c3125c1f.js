68f8f18d6332497ad1daed4223af62df9ca266be
Nishith Patnaik <np@Nishiths-MacBook-Air.local>
2021-09-24 00:01:19 +0530

    added build directory

---
+++ client/build/precache-manifest.02e1a6e242f2b09f18faef87c3125c1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f5449a56fba85b674da6ba534c9bbeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e3e1655fb40b2ea85c6f",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "9aec659a06c03a6faefb",
+    "url": "/static/js/2.66f58f3c.chunk.js"
+  },
+  {
+    "revision": "e3e1655fb40b2ea85c6f",
+    "url": "/static/js/main.d137aa5c.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  }
+]);
